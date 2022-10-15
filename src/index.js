de215c666e738efa39097e0bd05e5437fec22b84
Mehrdad Dindar <mehrdad.replay@gmail.com>
2022-10-15 09:44:52 +0330

    create First React Timer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('main'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const tick = () => {
+  const elem = (
+      <div>
+          <h1>First React Timer</h1>
+          <h2>Time is : {new Date().toLocaleTimeString()}</h2>
+      </div>
+  );
+    root.render(
+        elem
+    );
+}
+setInterval(()=>{
+
+    tick();
+},1000);
